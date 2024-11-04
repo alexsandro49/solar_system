97f3b000bbee40f6849c3902e3845c3f456efe9e
Alex Sandro <alexsandro49.dev@gmail.com>
2024-11-04 00:35:21 -0300

    feat: solar system

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# sliding_block_puzzle
+## "Mercury (planet)" (https://skfb.ly/6yuRD) by SebastianSosnowski is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## "Venus" (https://skfb.ly/ITvT) by kongle is licensed under Creative Commons Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/4.0/).
+
+## "Planet Earth" (https://skfb.ly/owwQH) by JanesBT is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## Planet Mars - NASA Mars Landing - 2021 CC0 Public DomainCC0
+
+## "Realistic Jupiter" (https://skfb.ly/oKT8Y) by Shady Tex is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## "Saturn (planet)" (https://skfb.ly/6yw9I) by SebastianSosnowski is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## "Uranus" (https://skfb.ly/6TwIN) by Akshat is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## "Neptune" (https://skfb.ly/6ABCv) by SebastianSosnowski is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+## "Sun" (https://skfb.ly/6yGSx) by SebastianSosnowski is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
--- main.py
+++ main.py
@@ -1,82 +1,55 @@
-from ursina import (
-    Ursina,
-    Entity,
-    Sky,
-    EditorCamera,
-    scene,
-    camera,
-)
-from piece_cube import PieceCube
-from rubik_cube import RubikCube
+from ursina import Ursina, Entity, EditorCamera, color, time
+from ursina.prefabs.sky import Sky
+import numpy as np
 
-app = Ursina(
-    borderless=False, fullscreen=False, development_mode=True, editor_ui_enabled=True
-)
+app = Ursina(borderless=False, fullscreen=False, development_mode=True, editor_ui_enabled=True)
 
-Sky()
 EditorCamera()
+Sky()
 
-MODEL = "models/custom_cube.obj"
-TEXTURE = "textures/rubik_texture.png"
-
-PARENT = PieceCube(
-    model=MODEL,
-    position=(0, 0, 0),
-    texture="noise",
-)
-
-rubik_cube = RubikCube(PARENT)
+t = -np.pi
 
 
-piece_positions = [
-    (-1, -1, -1),
-    (-1, -1, 0),
-    (-1, -1, 1),
-    (0, -1, -1),
-    (0, -1, 0),
-    (0, -1, 1),
-    (1, -1, -1),
-    (1, -1, 0),
-    (1, -1, 1),
-    (-1, 0, -1),
-    (0, 0, -1),
-    (1, 0, -1),
-    (-1, 0, 0),
-    (1, 0, 0),
-    (-1, 0, 1),
-    (0, 0, 1),
-    (1, 0, 1),
-    (-1, 1, -1),
-    (0, 1, -1),
-    (1, 1, -1),
-    (-1, 1, 0),
-    (0, 1, 0),
-    (1, 1, 0),
-    (-1, 1, 1),
-    (0, 1, 1),
-    (1, 1, 1),
-]
 
+sun = Entity(model='sphere', scale=1.5)
 
-for piece_position in piece_positions:
-    cube = PieceCube(
-        model=MODEL,
-        texture=TEXTURE,
-        parent=scene,
-        position=piece_position,
-    )
+mercury = Entity(model='sphere', scale=0.2)
+venus = Entity(model='sphere', scale=0.3)
+earth = Entity(model='sphere', scale=0.4)
+mars = Entity(model='sphere', scale=0.3)
+jupiter = Entity(model='sphere', scale=0.6)
+saturn = Entity(model='sphere', scale=0.5)
+uranus = Entity(model='sphere', scale=0.5)
+neptune = Entity(model='sphere', scale=0.5)
 
-    rubik_cube.add_piece(cube)
+def update():
+    global t
+    angle = np.pi*40/180
 
-Entity(
-    model="quad",
-    scale=60,
-    texture="white_cube",
-    texture_scale=(60, 60),
-    rotation_x=90,
-    y=-5,
-)
+    t += 0.01 #* time.dt
 
-camera.position = (0, 0, 0)
+    mercury.x = np.cos(t) * 1
+    mercury.z = np.sin(t) * 1
+
+    venus.x = np.cos(t+angle)* 1.4
+    venus.z = np.sin(t+angle)* 1.4    
+
+    earth.x = np.cos(t+angle*2)* 1.8
+    earth.z = np.sin(t+angle*2)* 1.8
+
+    mars.x = np.cos(t+angle*3)*2.2
+    mars.z = np.sin(t+angle*3)*2.2
+
+    jupiter.x = np.cos(t+angle*4)*2.6
+    jupiter.z = np.sin(t+angle*4)*2.6
+    
+    saturn.x = np.cos(t+angle*5)* 3
+    saturn.z = np.sin(t+angle*5)* 3  
+
+    uranus.x = np.cos(t+angle*6)* 3.4
+    uranus.z = np.sin(t+angle*6)* 3.4  
+
+    neptune.x = np.cos(t+angle*7)* 3.8
+    neptune.z = np.sin(t+angle*7)* 3.8  
 
 app.run()
